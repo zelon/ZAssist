6aac36e56a5745977adb62b0ca2ec0be554ab0ab
zelonion <zelonion@0d94c7d9-cc4c-0410-a681-d3a157e881fb>
2008-05-12 15:02:53 +0000

    Wrapper class. from HWND to IWin32Window
    git-svn-id: http://zassist.googlecode.com/svn/trunk@16 0d94c7d9-cc4c-0410-a681-d3a157e881fb

---
+++ ZAssist/ZAssist/WindowWrapper.cs
@@ -0,0 +1,24 @@
+﻿/// http://www.ryanfarley.com/blog/archive/2004/03/23/465.aspx
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZAssist
+{
+    public class WindowWrapper : IWin32Window
+    {
+        public WindowWrapper(IntPtr handle)
+        {
+            _hwnd = handle;
+        }
+
+        public IntPtr Handle
+        {
+            get { return _hwnd; }
+        }
+
+        private IntPtr _hwnd;
+    }
+}
