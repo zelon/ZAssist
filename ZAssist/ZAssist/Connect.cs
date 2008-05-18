b76dfb99106840209252b482ab3e48f50cc67077
zelonion <zelonion@0d94c7d9-cc4c-0410-a681-d3a157e881fb>
2008-05-18 17:06:12 +0000

    Starting split feature method
    git-svn-id: http://zassist.googlecode.com/svn/trunk@22 0d94c7d9-cc4c-0410-a681-d3a157e881fb

--- ZAssist/ZAssist/Connect.cs
+++ ZAssist/ZAssist/Connect.cs
@@ -205,49 +205,7 @@ namespace ZAssist
                 {
                     if (commandName == "ZAssist.Connect.OpenCorrespondingFile")
                     {
-                        if (_applicationObject.ActiveDocument != null)
-                        {
-                            string filename = System.IO.Path.GetFullPath(_applicationObject.ActiveDocument.FullName);
-
-                            if (filename.ToLower().EndsWith(".h") || filename.ToLower().EndsWith(".cpp") || filename.ToLower().EndsWith(".c"))
-                            {
-                                string newfilename = System.IO.Path.GetDirectoryName(filename) + "\\" + System.IO.Path.GetFileNameWithoutExtension(filename);
-
-                                switch (System.IO.Path.GetExtension(filename).ToLower())
-                                {
-                                    case ".h":
-                                        newfilename += ".cpp";
-                                        break;
-
-                                    case ".cpp":
-                                        newfilename += ".h";
-                                        break;
-
-                                    case ".c":
-                                        newfilename += ".h";
-                                        break;
-                                }
-
-                                if (File.Exists(newfilename))
-                                {
-                                    Window w = _applicationObject.OpenFile(EnvDTE.Constants.vsViewKindPrimary, newfilename);
-
-                                    if (w != null)
-                                    {
-                                        w.Activate();
-                                    }
-                                }
-                                else
-                                {
-                                    System.Diagnostics.Debug.Print("There is no file : " + newfilename);
-                                }
-
-                            }
-                        }
-                        else
-                        {
-                            System.Diagnostics.Debug.Print("ActiveDocument is null");
-                        }
+                        ZAssistManager.OpenCorrespondingFile(_applicationObject);
 
                         handled = true;
                         return;
---
+++ ZAssist/ZAssist/ZAssistManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using EnvDTE;
+using EnvDTE80;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZAssist
+{
+    class ZAssistManager
+    {
+        internal static void OpenCorrespondingFile(DTE2 _applicationObject)
+        {
+            if (_applicationObject.ActiveDocument != null)
+            {
+                string filename = System.IO.Path.GetFullPath(_applicationObject.ActiveDocument.FullName);
+
+                if (filename.ToLower().EndsWith(".h") || filename.ToLower().EndsWith(".cpp") || filename.ToLower().EndsWith(".c"))
+                {
+                    string newfilename = System.IO.Path.GetDirectoryName(filename) + "\\" + System.IO.Path.GetFileNameWithoutExtension(filename);
+
+                    switch (System.IO.Path.GetExtension(filename).ToLower())
+                    {
+                        case ".h":
+                            newfilename += ".cpp";
+                            break;
+
+                        case ".cpp":
+                            newfilename += ".h";
+                            break;
+
+                        case ".c":
+                            newfilename += ".h";
+                            break;
+                    }
+
+                    if (File.Exists(newfilename))
+                    {
+                        Window w = _applicationObject.OpenFile(EnvDTE.Constants.vsViewKindPrimary, newfilename);
+
+                        if (w != null)
+                        {
+                            w.Activate();
+                        }
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.Print("There is no file : " + newfilename);
+                    }
+
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.Print("ActiveDocument is null");
+            }
+        }
+    }
+}
